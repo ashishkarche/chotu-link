dac57dcaa5d12e514b69e4ebb7eb4e53837b77a4
ashishkarche <ashishkarche9@gmail.com>
2025-08-21 12:40:23 +0530

    Small Update

--- chotu-link-frontend/src/componets/Footer.js
+++ chotu-link-frontend/src/componets/Footer.js
@@ -3,16 +3,14 @@ function Footer() {
     <footer className="bg-light text-center text-lg-start border-top py-3 mt-auto">
       <div className="container d-flex flex-column flex-md-row justify-content-between align-items-center">
         <p className="mb-2 mb-md-0 text-muted small">
-          © {new Date().getFullYear()} <strong>ChotuLink</strong>. All Rights Reserved.
+          © {new Date().getFullYear()} <strong>ChotuLink | Ashish Karche</strong>. All Rights Reserved.
         </p>
         <div>
-          <a href="https://twitter.com" className="text-muted me-3">
-            <i className="bi bi-twitter fs-5"></i>
+          <a href="https://github.com/ashishkarche/chotu-link" className="text-muted me-3">
+            <i className="bi bi-github fs-5"></i>
           </a>
-          <a href="https://facebook.com" className="text-muted me-3">
-            <i className="bi bi-facebook fs-5"></i>
-          </a>
-          <a href="https://linkedin.com" className="text-muted">
+
+          <a href="https://www.linkedin.com/in/ashish-karche-1a422b317/" className="text-muted">
             <i className="bi bi-linkedin fs-5"></i>
           </a>
         </div>
